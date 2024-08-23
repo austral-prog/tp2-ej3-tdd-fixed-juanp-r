547be1a43064e23b37532a10d6e39f0130301109
Juan Prieto <162722098+juanp-r@users.noreply.github.com>
2024-08-23 10:52:55 -0300

    App.java

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -1,14 +1,32 @@
 package com.template;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class App {
 
 
     public static Map<String, Integer> deposit(Map<String, Integer> accounts, String account, int amount) {
-        return Collections.emptyMap();
+        if (amount > 0 && accounts.containsKey(account)) {
+             accounts.put(account, accounts.get(account) + amount);
+        }
+        return accounts;
     }
 
+    public static Map<String,Integer> withdraw(Map<String, Integer> accounts, String account , int amount) {
+        if (amount > 0 && accounts.containsKey(account) && amount < accounts.get(account)) {
+            accounts.put(account, accounts.get(account) - amount);
+        }
+        return accounts;
+    }
+
+    public static Map<String,Integer> transfer(Map<String, Integer> accounts,String sender,String receiver, int amount) {
+        if (amount > 0 && accounts.containsKey(sender) && accounts.containsKey(receiver) && amount <= accounts.get(sender) && sender != receiver) {
+            accounts.put(receiver, accounts.get(receiver) + amount);
+            accounts.put(sender, accounts.get(sender) - amount);
+        }
+        return accounts;
+    }
 
 }
